b6d19e35d6b4426afd7a796eaf8cec8a6219541f
Vinícius Yoda <vyoda4604@gmail.com>
2023-06-13 15:53:35 -0300

    sql

---
+++ 17.sql
@@ -0,0 +1,12 @@
+USE `sucos_vendas`;
+DROP procedure IF EXISTS `acha_sabor_produto`;
+DELIMITER $$
+
+USE `sucos_vendas`$$
+CREATE PROCEDURE `acha_sabor_produto`(vProduto VARCHAR(50))
+BEGIN
+   DECLARE vSabor VARCHAR(50);
+   SELECT SABOR INTO vSabor FROM tabela_de_produtos WHERE codigo_do_produto = vProduto;
+   SELECT vSabor;
+END$$
+DELIMITER ;
